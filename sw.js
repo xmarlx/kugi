5b78783d60a2190bab27b5cce9374d43246ce68b
xmarlx <47490347+xmarlx@users.noreply.github.com>
2026-09-24 19:18:04 +0900

    add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C="kugi-v1",F=["./","index.html","manifest.json","icon-192.png","icon-512.png","icon-180.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(e.request.method!=="GET"||u.origin!==location.origin)return;
+e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r}).catch(()=>caches.match(e.request)))});
